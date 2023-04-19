bcd52ce59c54ece8180ab4a948a6ccd5cbe09400
Pablo Baleztena <pablo@ultharsoftware.com>
2023-04-19 10:09:41 -0300

    Add cbatticon and nm-applet to notebook config

--- awesome/.config/awesome/user-autostart.lua
+++ awesome/.config/awesome/user-autostart.lua
@@ -9,9 +9,12 @@ local function autostart_apps()
     run_once("lxpolkit")
     run_once("gnome-keyring-daemon", "gnome-keyring-daemon --start --components=pkcs11,secrets,ssh")
 
+
     -- Other tools
     run_once("thunar --daemon")
 
+    run_once("cbatticon")
+    run_once("nm-applet")
     run_once("flameshot")
     run_once("megasync")
     run_once("blueman-applet")
